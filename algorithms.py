71af2ab8aae3f92f81f29026d99cd78229935bfe
Sherice7777 <sweetnotes88@yahoo.com>
2026-04-29 10:21:33 -0500

    Add files via upload

---
+++ algorithms.py
@@ -0,0 +1,2 @@
+# Write your CUSTOM search algorithm (a function) and an sorting algorithm (a function) AS DESCRIBED IN THE ASSIGNMENT INSTRUCTIONS here
+# You MAY NOT USE any pre-built python functions
